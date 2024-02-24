0b88c056fc0698da1c96384c1b47e9c8d2b613e0
Badis Ghoubali <110173477+BadisG@users.noreply.github.com>
2024-02-24 18:58:52 +0100

    Create Improve-youtube-searsh.user.js

---
+++ Improve-youtube-searsh.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name     Improve-youtube-searsh 
+// @version  1
+// @grant    none
+// @match    https://*.youtube.com/*
+// ==/UserScript==
+
+window.onload = function() {
+    let searchInput = document.querySelector('input#search');
+    if (searchInput) {
+        searchInput.addEventListener('keydown', function(e) {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                if (!searchInput.value.includes('after:2004')) {
+                    searchInput.value += ' after:2004';
+                }
+            }
+        });
+    }
+};
